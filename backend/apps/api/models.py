7b9e0fc7b996fb35da3380640f404c6ce894816e
malte_then <malte.then@geodock.de>
2025-01-28 23:13:24 +0100

    Add PostgreSQL support and define Trench data models with GIS integration

--- backend/apps/api/models.py
+++ backend/apps/api/models.py
@@ -1,3 +1,35 @@
+import uuid
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+from django.conf import settings
 
-# Create your models here.
+
+class AttributesSurface(models.Model):
+    surface = models.TextField(null=False)
+    sealing = models.BooleanField(null=False)
+
+
+class AttributesConstructionType(models.Model):
+    construction_type = models.TextField(null=False)
+
+
+class Trench(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    id = models.IntegerField(null=False)
+    surface = models.ForeignKey(AttributesSurface, null=False, on_delete=models.CASCADE)
+    construction_type = models.ForeignKey(
+        AttributesConstructionType, null=False, on_delete=models.CASCADE
+    )
+    construction_depth = models.IntegerField(null=True)
+    construction_details = models.TextField(null=True)
+    status = models.TextField(null=True)
+    phase = models.TextField(null=True)
+    internal_execution = models.BooleanField(null=True)
+    funding_status = models.BooleanField(null=True)
+    owner = models.TextField(null=True)
+    company = models.TextField(null=True)
+    date = models.DateField(null=True)
+    comment = models.TextField(null=True)
+    house_connection = models.BooleanField(null=True, default=False)
+    length = models.DecimalField(null=False, max_digits=8, decimal_places=4)
+    geom = gis_models.LineStringField(null=False, srid=settings.DEFAULT_SRID)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
     "apps.api",
     "rest_framework",
 ]
@@ -116,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "de-de"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
@@ -134,3 +135,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Default SRID for geometry fields
+DEFAULT_SRID = 25832
