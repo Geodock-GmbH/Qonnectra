6a026746a82df1e30d9da1e00476130a96f7fc23
malte_then <malte.then@geodock.de>
2025-02-04 21:36:19 +0100

    Add Docker Compose configuration and initialization script for PostGIS database

---
+++ deployment/postgres/init.sql
@@ -0,0 +1,10 @@
+-- Change these to your desired values
+
+-- The credentials are not meant to be used in production!
+-- Create the database
+CREATE DATABASE krit_gis;
+CREATE USER geodock_admin WITH PASSWORD 'geodock_admin';
+ALTER ROLE geodock_admin SET client_encoding TO 'utf8';
+ALTER ROLE geodock_admin SET default_transaction_isolation TO 'read committed';
+ALTER ROLE geodock_admin SET timezone TO 'Europe/Berlin';
+GRANT ALL PRIVILEGES ON DATABASE krit_gis TO geodock_admin;
