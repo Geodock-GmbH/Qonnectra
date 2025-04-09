f3f07e3c02bc0a90ac5f4c3975d40dfd896e4bb9
m-then <malte.then@geodock.de>
2025-04-09 12:46:43 +0200

    feat: Moved sidebar over appbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,11 +5,11 @@
 	let { children } = $props();
 </script>
 
-<div class="h-screen flex flex-col">
+<div class="flex flex-col relative">
 	<AppBar />
-	<div class="flex flex-1">
+	<div class="h-screen flex flex-1 absolute top-0">
 		<Sidebar />
-		<main class="flex-1 p-4">
+		<main class="flex-1 p-4 mt-16">
 			{@render children()}
 		</main>
 	</div>
