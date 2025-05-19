45fae09c59fda0b6267db3bd018a21e3230001fa
Malte Then <malte.then@geodock.de>
2025-05-19 14:38:53 +0200

    feat: Enhance settings page withtranslations

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -29,18 +29,26 @@
 		<div class="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
 			<!-- User Settings -->
 			<div>
-				<h2 class="text-base/7 font-semibold">User</h2>
-				<p class="mt-1 text-sm/6">Here you can see your user settings.</p>
+				<h2 class="text-base/7 font-semibold">
+					{m.settings_user()}
+				</h2>
+				<p class="mt-1 text-sm/6">
+					{m.settings_user_description()}
+				</p>
 
 				<dl class="mt-6 divide-y border-t text-sm/6">
 					<div class="py-6 sm:flex">
-						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Username</dt>
+						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">
+							{m.settings_user_username()}
+						</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							<div>{$userStore.username}</div>
 						</dd>
 					</div>
 					<div class="py-6 sm:flex">
-						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Email address</dt>
+						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">
+							{m.settings_user_email()}
+						</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							<div>{$userStore.email}</div>
 						</dd>
@@ -50,12 +58,18 @@
 
 			<!-- UI Settings -->
 			<div>
-				<h2 class="text-base/7 font-semibold">UI</h2>
-				<p class="mt-1 text-sm/6">Here you can see and change your user interface settings.</p>
+				<h2 class="text-base/7 font-semibold">
+					{m.settings_ui()}
+				</h2>
+				<p class="mt-1 text-sm/6">
+					{m.settings_ui_description()}
+				</p>
 
 				<dl class="mt-6 divide-y border-t text-sm/6">
 					<div class="py-6 sm:flex">
-						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Default Project</dt>
+						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">
+							{m.settings_ui_default_project()}
+						</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							<span>{globalThis.$defaultProject}</span>
 							<button
@@ -70,9 +84,9 @@
 						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Sidebar</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							{#if $sidebarExpanded}
-								<p>Expanded</p>
+								<p>{m.settings_ui_sidebar_expanded()}</p>
 							{:else}
-								<p>Collapsed</p>
+								<p>{m.settings_ui_sidebar_collapsed()}</p>
 							{/if}
 							<Switch
 								name="sidebar-expanded"
@@ -89,11 +103,15 @@
 			<!-- TODO: Add Map Settings -->
 			<div>
 				<h2 class="text-base/7 font-semibold">Map</h2>
-				<p class="mt-1 text-sm/6">Here you can see and change your map settings.</p>
+				<p class="mt-1 text-sm/6">
+					{m.settings_map_description()}
+				</p>
 
 				<dl class="mt-6 divide-y border-t text-sm/6">
 					<div class="py-6 sm:flex">
-						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Trench color</dt>
+						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">
+							{m.settings_map_trench_color()}
+						</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							<input type="color" bind:value={$trenchColor} />
 							<button
@@ -109,7 +127,9 @@
 						</dd>
 					</div>
 					<div class="py-6 sm:flex">
-						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">Selected trench color</dt>
+						<dt class="font-medium sm:w-64 sm:flex-none sm:pr-6">
+							{m.settings_map_selected_trench_color()}
+						</dt>
 						<dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
 							<input type="color" bind:value={$trenchColorSelected} />
 							<button
