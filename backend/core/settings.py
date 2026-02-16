395fd71fec681f087deb68aafd181d920d97d519
Malte Then <malte.then@geodock.de>
2026-02-16 21:26:54 +0100

    feat: Update REST framework settings to include JSONRenderer and conditionally add BrowsableAPIRenderer based on DEBUG

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -203,10 +203,10 @@ REST_FRAMEWORK = {
         # Use JWTCookieAuthentication for cookie-based JWT auth
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
-    # Add permission settings if needed globally, otherwise handle per-view
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    # ]
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ]
+    + (["rest_framework.renderers.BrowsableAPIRenderer"] if DEBUG else []),
 }
 
 # dj-rest-auth settings
