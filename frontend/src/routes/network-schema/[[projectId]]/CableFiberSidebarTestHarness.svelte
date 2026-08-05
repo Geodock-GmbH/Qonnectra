bb6999514dbe1e1e067a4d0244be4ac01eb378ad
Malte Then <malte.then@geodock.de>
2026-08-05 20:52:01 +0200

    test: Cover cable fiber sidebar and node slot config panel

---
+++ frontend/src/routes/network-schema/[[projectId]]/CableFiberSidebarTestHarness.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { setContext } from 'svelte';
+
+	import { DRAG_DROP_CONTEXT_KEY } from '$lib/classes/DragDropManager.svelte';
+
+	import CableFiberSidebar from './CableFiberSidebar.svelte';
+
+	let { dragDropManager = null, ...rest } = $props();
+
+	setContext(DRAG_DROP_CONTEXT_KEY, dragDropManager);
+</script>
+
+<CableFiberSidebar {...rest} />
